f825345f9927359431a692e63b671bf7aa7bf308
wangketao2010 <31028807+wangketao2010@users.noreply.github.com>
2018-12-19 20:13:39 +0800

    Add files via upload
    有一个问题没有解决就是，用Docker-Maven-plugin 编译的时候，输出文件没法复制到src/main/docker 路径下面，自己拖过去吧
    不知道安装本地的Docker服务器会不会出现这个问题，我连接的是虚拟机的远程Docker服务器所以有问题

---
+++ dockerTest/src/main/java/com/ktwang/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ktwang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "hello kt";
+    }
+}
